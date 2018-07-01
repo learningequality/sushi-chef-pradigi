4220bd490447d25058cd26e9d0d065e21b5f6a24
Ivan Savov <ivan.savov@gmail.com>
2018-07-01 15:23:37 -0400

    new youtube stuff

---
+++ update_youtube_info_utils.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+echo 'Copying latest youtube.py from kollivier github'
+git clone git@github.com:kollivier/pressurecooker.git
+cd pressurecooker/
+git checkout youtube_info
+cp pressurecooker/youtube.py ../youtube.py
+cd ..
+rm -rf pressurecooker
+

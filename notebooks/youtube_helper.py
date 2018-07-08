5a04723eff98de1baa9d49c21616506bea1c5ddd
Ivan Savov <ivan.savov@gmail.com>
2018-07-08 14:02:33 +0300

    Youtube helper renamed

---
+++ notebooks/youtube_helper.py
@@ -0,0 +1,1 @@
+../youtube_helper.py
--- update_youtube_helper.sh
+++ update_youtube_helper.sh
@@ -5,7 +5,7 @@ echo 'Copying latest youtube.py from kollivier github'
 git clone git@github.com:kollivier/pressurecooker.git
 cd pressurecooker/
 git checkout youtube_info
-cp pressurecooker/youtube.py ../youtube.py
+cp pressurecooker/youtube.py ../youtube_helper.py
 cd ..
 rm -rf pressurecooker
 
